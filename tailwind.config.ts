b44759e7d4b9d7426637427f30c64dffdb1e25ff
Burner481 <Burnerondeck481@gmail.com>
2025-11-29 01:33:50 +0100

    La til tolike-final og oppdatert oppsett for spillet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   theme: {
     container: {
       center: true,
@@ -25,17 +25,17 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        'match-celebrate': {
-          '0%, 100%': { transform: 'scale(1)', },
-          '50%': { transform: 'scale(1.1)', },
-        }
+        "match-celebrate": {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        'match-celebrate': 'match-celebrate 0.4s ease-in-out',
+        "match-celebrate": "match-celebrate 0.4s ease-in-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [],
+};
